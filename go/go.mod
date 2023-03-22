ced95b090f8427a8c8c15ba61a4778f0ffeed1a5
kumagai <kumagai.yuki@servithink.co.jp>
2023-03-22 09:04:47 +0000

    2023/03/22 18:03 fixed - main.goの修正 - docker-composeの修正 - ワークフローの修正

--- go/go.mod
+++ go/go.mod
@@ -3,25 +3,29 @@ module github.com/kumagai-s/uploader-v2
 go 1.19
 
 require (
-	github.com/aws/aws-sdk-go v1.44.195 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.17.4 // indirect
+	github.com/aws/aws-lambda-go v1.38.0
+	github.com/aws/aws-sdk-go-v2 v1.17.6
+	github.com/slack-go/slack v0.12.1
+)
+
+require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.18.12 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.12 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.22 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.28 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.22 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.29 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.19 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.17 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.17 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.30 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.31 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.22 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.23 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.22 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.22 // indirect
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.30.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.12.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.18.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.30.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.6 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/slack-go/slack v0.12.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- go/main.go
+++ go/main.go
@@ -11,107 +11,163 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
 
-var api = slack.New("TOKEN")
+var (
+	slackClient     *slack.Client
+	s3Client        *s3.Client
+	s3PresignClient *s3.PresignClient
+)
 
-func main() {
+func init() {
+	slackClient = slack.New(os.Getenv("SLACK_OAUTH_TOKEN"))
+
+	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			PartitionID:   "aws",
+			URL:           os.Getenv("S3_URL"),
+			SigningRegion: os.Getenv("S3_REGION"),
+		}, nil
+	})
+	sdkconfig, _ := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolver(customResolver))
+	s3Client = s3.NewFromConfig(sdkconfig, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
+
+	s3PresignClient = s3.NewPresignClient(s3Client)
+}
+
+func lambdaHandler(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	body := r.Body
+	headers := r.Headers
+	log.Println("リクエストヘッダー", headers)
+	log.Println("リクエストボディ", body)
+
+	// Slackのリトライリクエストは無視する。
+	if headers["X-Slack-Retry-Num"] != "" {
+		return events.APIGatewayProxyResponse{StatusCode: 200, Body: "No need retry"}, nil
+	}
+
+	// SlackAPIのシークレットキーを用いて検証する。
 	signingSecret := os.Getenv("SLACK_SIGHNG_SECRET")
+	header := http.Header{}
+	for key, value := range headers {
+		header.Set(key, value)
+	}
+	sv, err := slack.NewSecretsVerifier(header, signingSecret)
+	if err != nil {
+		log.Println("検証中にエラーが発生しました。", err)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Bad Request"}, err
+	}
+	if _, err := sv.Write([]byte(body)); err != nil {
+		log.Println("検証中にエラーが発生しました。", err)
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Bad Request"}, err
+	}
+	if err := sv.Ensure(); err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 401, Body: "Unauthorized"}, err
+	}
 
-	http.HandleFunc("/uploader-v2/upload", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if _, err := sv.Write(body); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if err := sv.Ensure(); err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+	if err != nil {
+		log.Println("リクエストの解析中にエラーが発生しました。", err)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal Server Error"}, err
+	}
 
-		if eventsAPIEvent.Type == slackevents.URLVerification {
-			var r *slackevents.ChallengeResponse
-			err := json.Unmarshal([]byte(body), &r)
+	// SlackAPIのURL検証用
+	if eventsAPIEvent.Type == slackevents.URLVerification {
+		var cr *slackevents.ChallengeResponse
+		if err := json.Unmarshal([]byte(body), &cr); err != nil {
+			log.Println("SlackAPIからのURL検証用中にエラーが発生しました。", err)
+			return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Internal Server Error"}, err
+		}
+		return events.APIGatewayProxyResponse{StatusCode: 200, Body: cr.Challenge}, nil
+	}
+
+	// メイン処理
+	if eventsAPIEvent.Type == slackevents.CallbackEvent {
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			tsFrom, _ := strconv.ParseInt(ev.TimeStamp, 10, 64)
+			tsTo, _ := strconv.ParseInt(ev.EventTimeStamp, 10, 64)
+
+			// Slackからファイル情報を取得する。
+			files, _, err := slackClient.GetFiles(slack.GetFilesParameters{
+				User:          ev.User,
+				Channel:       ev.Channel,
+				TimestampFrom: slack.JSONTime(tsFrom),
+				TimestampTo:   slack.JSONTime(tsTo),
+			})
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
+				log.Println("Slackからファイル情報を取得中にエラーが発生しました。", err)
+				return events.APIGatewayProxyResponse{StatusCode: 400, Body: "Bad Request"}, err
 			}
-			w.Header().Set("Content-Type", "text")
-			w.Write([]byte(r.Challenge))
-		}
-		if eventsAPIEvent.Type == slackevents.CallbackEvent {
-			innerEvent := eventsAPIEvent.InnerEvent
-			switch ev := innerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
-				timestamp, _ := strconv.ParseInt(ev.TimeStamp, 10, 64)
-				files, _, err := api.GetFiles(slack.GetFilesParameters{
-					User:          ev.User,
-					Channel:       ev.Channel,
-					TimestampFrom: slack.JSONTime(timestamp),
+
+			for _, file := range files {
+				url := file.URLPrivateDownload
+
+				// Slackからファイルを取得する。
+				res, err := http.Get(url)
+				if err != nil {
+					log.Println("Slackからファイルを取得中にエラーが発生しました。", err)
+					continue
+				}
+				defer res.Body.Close()
+
+				file, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					log.Println("Slackからファイルを取得中にエラーが発生しました。", err)
+					continue
+				}
+
+				filename := time.Now().Format("20060102150405") + ".zip"
+
+				// ファイルをS3にアップロードする。
+				if _, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+					Bucket:      aws.String(os.Getenv("S3_BUCKET")),
+					Key:         aws.String(filename),
+					Body:        bytes.NewReader(file),
+					ContentType: aws.String("application/zip"),
+				}); err != nil {
+					log.Println("ファイルをS3にアップロード中にエラーが発生しました。", err)
+					continue
+				}
+
+				// 署名付きURLを生成する。
+				pr, err := s3PresignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
+					Bucket: aws.String(os.Getenv("S3_BUCKET")),
+					Key:    aws.String(filename),
+				}, func(opts *s3.PresignOptions) {
+					opts.Expires = time.Duration(60 * 3 * int64(time.Second))
 				})
 				if err != nil {
-					log.Fatal(err)
-					return
+					log.Println("署名付きURLを生成中にエラーが発生しました。", err)
+					continue
 				}
 
-				for _, file := range files {
-					url := file.URLPrivateDownload
-
-					res, err := http.Get(url)
-					if err != nil {
-						panic(err)
-					}
-					defer res.Body.Close()
-
-					customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-						if os.Getenv("APP_ENV") == "dev" {
-							return aws.Endpoint{
-								PartitionID:   "aws",
-								URL:           "http://minio:9000",
-								SigningRegion: "ap-northeast-1",
-							}, nil
-						}
-						return aws.Endpoint{}, &aws.EndpointNotFoundError{}
-					})
-					sdkconfig, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolver(customResolver))
-					s3Client := s3.NewFromConfig(sdkconfig, func(o *s3.Options) {
-						o.Credentials = aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider("root", "password", ""))
-						o.UsePathStyle = true
-					})
-					file, err := ioutil.ReadAll(res.Body)
-					_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-						Bucket:      aws.String("uploader-v2"),
-						Key:         aws.String(time.Now().Format("20060102150405") + ".zip"),
-						Body:        bytes.NewReader(file),
-						ContentType: aws.String("application/zip"),
-					})
-					if err != nil {
-						panic(err)
-					}
+				// Slackにメッセージを送信する。
+				if _, _, err := slackClient.PostMessage(
+					ev.Channel,
+					slack.MsgOptionText(pr.URL, false),
+					slack.MsgOptionTS(ev.TimeStamp),
+				); err != nil {
+					log.Println("Slackにメッセージを送信中にエラーが発生しました。", err)
+					continue
 				}
-				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
 			}
 		}
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+
+	return events.APIGatewayProxyResponse{StatusCode: 200, Body: "OK"}, nil
+}
+
+func main() {
+	lambda.Start(lambdaHandler)
 }
