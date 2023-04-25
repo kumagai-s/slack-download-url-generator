d63843e1487c5472d5e4d6552e3865da31cc95a0
kumagai <kumagai.yuki@servithink.co.jp>
2023-04-25 19:57:24 +0900

    2023/04/25 19:57 fixed - ファイル名をアップしたファイル名になるように修正

--- go/main.go
+++ go/main.go
@@ -103,7 +103,7 @@ func handleURLVerification(body string) (events.APIGatewayProxyResponse, error) 
 // 成功時には署名付きURLの文字列とnilのエラーを返します。
 // エラーが発生した場合、空文字列とエラーを返します。
 func uploadFileToS3AndGetPresignedURL(file *SlackAppMentionEventFile) (string, error) {
-	filename := time.Now().Format("20060102150405") + ".zip"
+	filename := file.Name + ".zip"
 
 	// ファイルをS3にアップロードする。
 	if _, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
